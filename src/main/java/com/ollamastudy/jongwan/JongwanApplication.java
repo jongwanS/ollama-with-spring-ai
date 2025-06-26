82a7eb321dd0e62e77be484e16838bdcfd63ef35
limjong-wan <lim8848@gmail.com>
2025-06-26 23:38:20 +0900

    Initial commit

---
+++ src/main/java/com/ollamastudy/jongwan/JongwanApplication.java
@@ -0,0 +1,13 @@
+package com.ollamastudy.jongwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JongwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JongwanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ollamastudy/jongwan/JongwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ollamastudy.jongwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JongwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
